e443c55da6998a1d61869426ca692020cf5ccbdb
SrijanAwale <srijanawale@gmail.com>
2025-03-30 14:02:38 -0500

    Finished product

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,5 @@
 import logo from "../assets/logo.png";
 //import classes from "../components/Header.module.css";
-import { styled } from "styled-components";
 
 export default function Header() {
   return (
@@ -8,9 +7,9 @@ export default function Header() {
       <img
         src={logo}
         alt="A canvas"
-        className="mb-8 w-44 h-44 object-contain"
+        className="object-contain mb-8 w-44 h-44"
       />
-      <h1 className=" text-xl md:text-4xl font-semibold tracking-widest text-center uppercase text-align">
+      <h1 className=" text-xl font-semibold font-display tracking-widest text-center uppercase md:text-4xl">
         ReactArt
       </h1>
       <p className="text-stone-500">A community of artists and art-lovers.</p>
